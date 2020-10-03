044f0f4ecf0feaf6d93d43f6f15555aa8fd9bcbf
BobbySinclusto <fishermanacb@gmail.com>
2020-10-03 11:06:03 -0700

    refactored stuff

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 window.onload = function(){
-
-
+  // Cool title animation
     new TypeIt("#typeme", {
         strings: "Contact Tracing by Hacky Stack",
         speed: 75,
         loop: true
       }).go();
+
+  // Setup sigma window
+  sigma.parsers.json('data.json', {
+    container: 'container',
+        settings: {
+            scalingMode: "inside",
+            sideMargin: 0,
+            autoResize: false,
+            autoRescale: true,
+            defaultNodeColor: '#ec5148'
+        }
+    });
 }
